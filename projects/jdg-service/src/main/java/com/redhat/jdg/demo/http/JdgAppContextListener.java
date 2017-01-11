8602fb1cd782cd040d66a08573a1312a7158c94f
serhat-dirik <serhat.dirik@gmail.com>
2017-01-11 23:44:03 +0300

    environment variable support in jdg server list value

--- projects/jdg-service/src/main/java/com/redhat/jdg/demo/http/JdgAppContextListener.java
+++ projects/jdg-service/src/main/java/com/redhat/jdg/demo/http/JdgAppContextListener.java
@@ -24,11 +24,14 @@ package com.redhat.jdg.demo.http;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.redhat.jdg.demo.resource.JdgResourcesConfig;
 
 public class JdgAppContextListener implements ServletContextListener {
 
+  private static Pattern envPattern = Pattern.compile("(?<=\\$\\{)(.*?)(?=\\})");
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		String jdg_serverList = sce.getServletContext().getInitParameter("jdg_serverList");
@@ -38,8 +41,15 @@ public class JdgAppContextListener implements ServletContextListener {
 		}
 		String jdg_userName = sce.getServletContext().getInitParameter("jdg_userName");
 		String jdg_password = sce.getServletContext().getInitParameter("jdg_password");
+    String jdg_serverList_replaced = jdg_serverList;
+		Matcher matcher = envPattern.matcher(jdg_serverList);
+		while(matcher.find()){
+			String group = matcher.group();
+			String env = System.getenv(group);
+			jdg_serverList_replaced = jdg_serverList_replaced.replace("${"+group+"}", env);
+		}
 
-		System.setProperty("infinispan.client.hotrod.server_list", jdg_serverList);
+		System.setProperty("infinispan.client.hotrod.server_list", jdg_serverList_replaced);
 		System.setProperty("infinispan.client.hotrod.cache", jdg_cache);
 		if(jdg_userName != null && jdg_password != null){
 			System.setProperty("infinispan.client.hotrod.username", jdg_userName);
